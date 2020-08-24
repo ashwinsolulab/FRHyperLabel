269749eabb33583dcc096097e40c63f0197592b7
Ashwin Kanjariya <ashwin@solulab.co>
2020-08-24 12:49:07 +0530

    pod updates for eezaroo

--- FRHyperLabel/FRHyperLabel.m
+++ FRHyperLabel/FRHyperLabel.m
@@ -27,8 +27,8 @@ static UIColor *FRHyperLabelLinkColorHighlight;
 
 + (void)initialize {
 	if (self == [FRHyperLabel class]) {
-		FRHyperLabelLinkColorDefault = [UIColor colorWithRed:28/255.0 green:135/255.0 blue:199/255.0 alpha:1];
-		FRHyperLabelLinkColorHighlight = [UIColor colorWithRed:242/255.0 green:183/255.0 blue:73/255.0 alpha:1];
+		FRHyperLabelLinkColorDefault = [UIColor colorWithRed:0/255.0 green:156/255.0 blue:157/255.0 alpha:1];
+		FRHyperLabelLinkColorHighlight = [UIColor colorWithRed:0/255.0 green:156/255.0 blue:157/255.0 alpha:1];
 	}
 }
 
@@ -58,13 +58,11 @@ static UIColor *FRHyperLabelLinkColorHighlight;
 	}
 	
 	if (!self.linkAttributeDefault) {
-		self.linkAttributeDefault = @{NSForegroundColorAttributeName: FRHyperLabelLinkColorDefault,
-									  NSUnderlineStyleAttributeName: @(NSUnderlineStyleSingle)};
+		self.linkAttributeDefault = @{NSForegroundColorAttributeName: FRHyperLabelLinkColorDefault};
 	}
 	
 	if (!self.linkAttributeHighlight) {
-		self.linkAttributeHighlight = @{NSForegroundColorAttributeName: FRHyperLabelLinkColorHighlight,
-										NSUnderlineStyleAttributeName: @(NSUnderlineStyleSingle)};
+		self.linkAttributeHighlight = @{NSForegroundColorAttributeName: FRHyperLabelLinkColorHighlight};
 	}
 }
 
